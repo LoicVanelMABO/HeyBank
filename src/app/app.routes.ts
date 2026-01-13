4b8e7c2307034afad38a0e03dad1e31cdd431265
Loic Vanel MABOMESI <loicvanelmabomesinkanko@gmail.com>
2026-01-13 14:35:34 +0100

    init app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TransactionsComponent } from './features/transactions/transactions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+      path: '',
+    component: TransactionsComponent,
+    title: 'Transactions Page',
+}];
