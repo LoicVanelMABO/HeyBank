ccfd728c148919a24f4495999d3868c420b6a08e
Charbel Sonon <104888004+AsKing07@users.noreply.github.com>
2026-01-14 13:49:20 +0100

    feat: mise à jour du composant d'en-tête avec des liaisons de route et amélioration de la structure HTML

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { AuthService } from './../../core/services/auth/auth.service';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink],
     templateUrl: './header.component.html',
-    styleUrl: './header.component.css'
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
     isMenuOpen = false;
+    isLoginedIn = signal(false);
+
+    constructor(authService: AuthService) {
+        this.isLoginedIn.set(authService.isLoggedIn());
+    }
 
     toggleMenu() {
         this.isMenuOpen = !this.isMenuOpen;
