69b30515f3e3d02d98622a04a474daae943c701f
Charbel Sonon <104888004+AsKing07@users.noreply.github.com>
2026-01-14 06:42:40 +0100

    feat: ajout de la protection des routes avec un guard d'authentification et ajout de la gestion de l'état de connexion dans le service d'authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./features/auth/login/login.component";
+import { RegisterComponent } from "./features/auth/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'heyBank';
+  _router: Router;
+
+  constructor(router: Router) {
+    this._router = router;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { RegisterComponent } from './features/auth/register/register.component';
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        title: 'Login',
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        title: 'Register',
+        path: 'register',
+        component: RegisterComponent
+    }
+];
