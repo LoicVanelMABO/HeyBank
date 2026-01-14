1b71b8a7557ddcb4fed225e8de076241f8c411ca
Charbel Sonon <104888004+AsKing07@users.noreply.github.com>
2026-01-14 14:06:47 +0100

    feat: ajout d'un bouton "Mon Profile" dans l'en-tête avec navigation et styles CSS

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,7 +1,7 @@
 import { AuthService } from './../../core/services/auth/auth.service';
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-header',
@@ -13,12 +13,17 @@ import { RouterLink } from "@angular/router";
 export class HeaderComponent {
     isMenuOpen = false;
     isLoginedIn = signal(false);
+    _router!: Router;
 
-    constructor(authService: AuthService) {
+    constructor(authService: AuthService, router: Router) {
         this.isLoginedIn.set(authService.isLoggedIn());
+        this._router = router;
     }
 
     toggleMenu() {
         this.isMenuOpen = !this.isMenuOpen;
+    }
+    navigateToProfile() {
+        this._router.navigate(['/profile']);
     }
 }
